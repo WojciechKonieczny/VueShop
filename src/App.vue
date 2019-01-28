47a0fd55a4b4c6a3b5e982d42e2d07d42b0b40d7
Wojciech Konieczny <36081180+WojciechKonieczny@users.noreply.github.com>
2019-01-28 21:51:41 +0100

    Add fonts, media style. Import components

--- src/App.vue
+++ src/App.vue
@@ -8,12 +8,55 @@
 </template>
 
 <style lang="scss">
-@import '@/assets/scss/global.scss';
 @import '@/assets/scss/variables.scss';
+@import url('https://fonts.googleapis.com/css?family=Roboto:400,500,700');
+
+@font-face {
+   font-family: 'Bebas Neue';
+   src: url('../src/assets/font/BebasNeue-Regular.ttf');
+}
+
+
+* {
+   padding: 0;
+   margin: 0;
+   box-sizing: border-box;
+}
+
+html {
+   width: 100%;
+   height: 100%;
+   font-size: 62.5%;
+   line-height: 1.312;
+}
+
+body {
+   width: 100%;
+   font-family: 'Roboto', sans-serif;
+   color: $primary-gray;
+   font-size: 100%;
+}
+
+.container {
+   width: 80%;
+   max-width: 90em;
+   margin: 0 auto;
+   background: #ffffff;
+}
+
+a:link, a:visited{
+   text-decoration: none;
+   color: $primary-gray;
+}
+
+@media all and (max-width: $l-screen) {
+   html {
+      font-size: 60%;
+   }
+}
 
 #app {
   background: $primary-gradient;
-  width: 100%;
   height: 100%;
   padding-top: 1.3em;
 }
@@ -21,8 +64,9 @@
 
 <script>
 import Menu from '@/components/Header/Menu';
+import Items from '@/components/Items/Items';
 
 export default {
-  components: { Menu },
+  components: { Menu, Items },
 }
 </script>
